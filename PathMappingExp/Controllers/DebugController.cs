1009a46fcc1fc134b110743d211c06b3c7588d78
Y.Arimitsu <yoshikazu_arimitsu@albert2005.co.jp>
2021-04-19 21:01:07 +0900

    プロジェクト ファイルを追加します。

---
+++ PathMappingExp/Controllers/DebugController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PathMappingExp.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class DebugController : ControllerBase {
+        readonly IConfiguration Config;
+
+        public DebugController(IConfiguration configuration) {
+            Config = configuration;
+        }
+
+        [HttpGet]
+        public string Get() {
+            return Config["ASPNETCORE_APPL_PATH"];
+        }
+    }
+}
